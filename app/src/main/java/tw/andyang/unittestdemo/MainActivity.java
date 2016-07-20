e5c94c8d6aac3e8bfde91ddb7d7267497425d02c
andyang <yang.bng86@gmail.com>
2016-07-20 16:51:01 +0800

    extract inner class to independent class

--- app/src/main/java/tw/andyang/unittestdemo/MainActivity.java
+++ app/src/main/java/tw/andyang/unittestdemo/MainActivity.java
@@ -114,27 +114,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    class Book {
-        private String name;
-
-        public Book(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Book book = (Book) o;
-
-            return name != null ? name.equals(book.name) : book.name == null;
-
-        }
-
-        @Override
-        public int hashCode() {
-            return name != null ? name.hashCode() : 0;
-        }
-    }
 }
